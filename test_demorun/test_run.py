919beaa56ed2b1fab1c232bc1f3742fd9bb798b6
mohshaik13@publicisgroupe.net <mohammed.shaik@publicissapient.com>
2025-03-28 14:44:53 +0530

    Pytest selenium demo

--- test_demorun/test_run.py
+++ test_demorun/test_run.py
@@ -3,6 +3,6 @@ from selenium import webdriver
 
 def test_demorun():
     driver=webdriver.Chrome()
-    driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+    driver.get("https://www.udemy.com/")
     print(driver.title)
     driver.close()
