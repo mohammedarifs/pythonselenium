2d97c421361486b18515c92cca80ccd963ea4825
mohshaik13@publicisgroupe.net <mohammed.shaik@publicissapient.com>
2025-03-28 16:15:53 +0530

    Pytest selenium demo

--- test_demorun/test_run.py
+++ test_demorun/test_run.py
@@ -1,8 +1,29 @@
+import pytest
 from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
 
 
-def test_demorun():
-    driver=webdriver.Chrome()
+@pytest.fixture()
+def set_driver():
+    yield webdriver.Chrome()
+
+def test_demorun(set_driver):
+
+    driver=set_driver
     driver.get("https://www.udemy.com/")
     print(driver.title)
+    driver.close()
+
+
+def test_searchtxt(set_driver):
+    driver=set_driver
+    driver.get("https://www.udemy.com/")
+    driver.find_element(By.XPATH,'//input[@data-testid="search-input"]').send_keys("Python selenium"+(Keys.RETURN))
+    driver.close()
+
+def test_closebrowser(set_driver):
+    driver=set_driver
+    driver.get("https://www.udemy.com/")
+    driver.find_element(By.XPATH,'//span[text()="Log in"]').click()
     driver.close()
